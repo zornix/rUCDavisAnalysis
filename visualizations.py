d70a41fafeff5809fa3353992ea1253228083b2a
jsandoval11 <jjsandoval@ucdavis.edu>
2026-03-11 20:47:04 -0700

    fix plot function

--- visualizations.py
+++ visualizations.py
@@ -77,14 +77,20 @@ def daysort_upvotes_vs_comments():
     plt.show()
 
 #Upvotes vs variables
-    def var_up(df):
-        vars = ["title_words", "selftext_words", "num_keywords", "num_comments"]
-        for var in vars:
-            upvotes_log = np.log2(df["upvotes"]+1)
-            plt.figure()
-            sns.set_theme(style = 'white', palette = 'Set2') 
-            plot = sns.scatterplot(
-                data=df,
-                x=var,
-                y=upvotes_log,
-            )
+def var_up(df):
+    variables = ["title_words", "selftext_words", "num_keywords", "num_comments"]
+    for var in variables:
+        upvotes_log = np.log2(df["upvotes"]+1)
+        plt.figure()
+        sns.set_theme(style = 'white', palette = 'Set2') 
+        plot = sns.scatterplot(
+            data=df,
+            x=var,
+            y= upvotes_log,
+        )
+        plot.set_title(f"Upvotes vs {var}")
+        plot.set_xlabel("upvotes")
+        plot.set_ylabel(var)
+
+        plt.show()
+        plt.close() 
